dffaf73757c22d3de1ee65e3384321e53b8fd825
Nour Shawky <nourshawky03@hotmail.com>
2025-12-04 19:16:27 -0800

    adding function signature and dock strings to download function

--- scripts/download_extract.py
+++ scripts/download_extract.py
@@ -1,24 +1,50 @@
+"""
+Downloading Raw Hotel Data
+
+This script reads hotel booking data from a given input path
+(URL or local CSV file) and saves a cleaned subset of columns to a specified output path 
+(data/raw folder)
+"""
+
 import pandas as pd
+import click
+import os
 
-# Download dataset
-data = pd.read_csv( 'https://raw.githubusercontent.com/manthangandhi/hotel_cancellation_prediction/refs/heads/main/data/hotel_bookings.csv')
+@click.command()
+@click.option("--input-path")
+@click.option("--output-path")
 
-# columns of interest
-cols_of_interest = [
-    "hotel",
-    "arrival_date_year",
-    "is_canceled", 
-    "lead_time",
-    "previous_cancellations",
-    "previous_bookings_not_canceled",
-    "booking_changes",
-    "total_of_special_requests",
-    "is_repeated_guest",
-    "deposit_type",
-    "reserved_room_type"
-]
+# Function for Downloading raw hotel dataset
+def download_extract(input_path, output_path):
+    """Hotel Data Downloader 
+    Reads in data from url/file path and saves it to the output path (in the data/raw folder)
 
-hotel_data = data[cols_of_interest]
+    Parameters
+    ----------
+    input_path : str
+        Path to input data (URL or csv file).
+    
+    output_path : str
+        File path to save the raw data to.
+    
+    Returns
+    --------
+    None: Function does not return anything, simply saves dataset to output path.
 
-# Save extracted dataframe to CSV
-hotel_data.to_csv("hotel_data.csv", index=False)
+    Examples:
+    ---------
+    From command line, run:
+
+    >>> python src/download_data.py --input-path https://exampleurl.com/data.csv --output-path data/raw/hotel_data.csv
+    >>> python src/download_data.py --input-path data/hotel_booking.csv --output-path data/raw/hotel_data.csv
+    """
+    
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    hotel_data = pd.read_csv(input_path)
+
+    hotel_data.to_csv(output_path, index=False)
+    click.echo(f"Saved data to: {output_path}")
+
+if __name__ == "__main__":
+    download_extract()
+
